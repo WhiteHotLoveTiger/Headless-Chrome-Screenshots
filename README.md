200cb2cca48064d93bc88a4c54a6ecbfff858e9a
Alex Keeling <keeling.alexander@brunswicknews.com>
2019-06-04 18:38:45 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Headless-Chrome-Screenshots
+
+Use Headless Chrome in AWS Lambda to take screenshots of webpages
+
+### Binaries
+There's two files here from other projects: 
+
+chromedriver comes from http://chromedriver.chromium.org/
+
+headless-chromium came from https://github.com/adieuadieu/serverless-chrome/releases
+
+It's important to make sure you've got compatible versions of these two files. The versions currently in the repo are chromedriver 2.37 and headless-chromium 64 (from serverless-chrome release 37).
+
+### Python Packages
+This project requires the selenium package from PyPI. It's in the requirements.txt file. Install it like this:
+
+`$ pip install -r requirements.txt -t .`
+
+### Packaging for Lambda
+`$ zip -r -9 lambda.zip lambda_function.py headless-chromium chromedriver selenium/ urllib3/`
+
+Upload the lambda.zip file to your function.
+
+### API Gateway
+This Lambda function is meant to go along with an API Gateway endpoint. Once you've got the lambda function in place, add in an API Gateway trigger.
+
+Because we're going to be returning images, be sure to allow binary media types. Enter `*/*` for this field.
+
+### Url Parameters
+The function implements these url parameters:
+ 
+ url - Which page to load. Use the full url including the protocol (http or https) and urlencode any special characters.
+  
+  - Good: `https%3A%2F%2Fgoogle.com`
+  
+  - Bad: `https://google.com`
+ 
+ res - The resolution of the screenshot. Defaults to 800 x 600.
+ 
+ wait - Integer seconds to wait for the page to load before taking the screenshot. Defaults to 1.
+ 
+ eg.
+ `?url=https%3A%2F%2Fgoogle.com&res=1000x800&wait=2`
+ 
---
+++ lambda_function.py
@@ -0,0 +1,77 @@
+import os
+import json
+import urllib.parse
+import subprocess as sp
+from time import sleep
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+default_res = ('800', '600')
+sp.getoutput('cp chromedriver /tmp/')
+sp.getoutput('cp headless-chromium /tmp/')
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=800x600')
+chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+chrome_options.add_argument('--hide-scrollbars')
+chrome_options.add_argument('--single-process')
+chrome_options.add_argument('--data-path=/tmp/data-path')
+chrome_options.add_argument('--homedir=/tmp')
+chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+chrome_options.binary_location = '/tmp/headless-chromium'
+
+driver = webdriver.Chrome(
+    executable_path=os.path.abspath('/tmp/chromedriver'),
+    options=chrome_options)
+
+
+def lambda_handler(event, context):
+    if 'queryStringParameters' in event and 'url' in event['queryStringParameters']:
+        url = urllib.parse.unquote(event['queryStringParameters']['url'])
+        if 'http' not in url:
+            url = 'http://' + url
+    else:
+        url = 'https://maritimedevcon.ca/'
+
+    res = default_res
+    if 'queryStringParameters' in event and 'res' in event['queryStringParameters']:
+        res = event['queryStringParameters']['res'].split('x')
+
+    nap = 1
+    if 'queryStringParameters' in event and 'wait' in event['queryStringParameters']:
+        nap = int(event['queryStringParameters']['wait'])
+
+    try:
+        driver.set_window_size(res[0], res[1])
+        driver.get(url)
+        sleep(nap)
+        b64_screenshot = driver.get_screenshot_as_base64()
+        return {
+            'statusCode': 200,
+            'body': b64_screenshot,
+            'isBase64Encoded': True,
+            'headers': {
+                'Content-Type': 'image/png',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    except Exception as e:
+        print(f'Problem with headless chrome. url: {url}\n{e}')
+        logger.exception(e)
+        return {
+            'statusCode': 500,
+            'body': 'Something went wrong :(',
+            'isBase64Encoded': False,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
